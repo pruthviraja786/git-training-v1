964ab6abbad5dbc870645dc4e2ffc2750c78e231
pruthviraja <pruthviraja.info@gmail.com>
2021-08-19 14:15:39 +0530

    2nd commit: modified ex1 and ex2 python files

---
+++ ex1-forloop.py
@@ -0,0 +1,11 @@
+# Count to n and print each number
+n = int(input("Enter the value of n: "))
+def count_to_n(x = n):
+    for i in range(1, x + 1):
+        print(i, end=' ')
+    print()
+
+print("Going to count to {} . . .".format(n))
+count_to_n(n)
+#------------------------------------------------------------------
+print("first modification for 2nd commit")
