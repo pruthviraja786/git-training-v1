2e1c37d449466f1acb37f4d48cc33bb8611fadee
pruthviraja786 <87389873+pruthviraja786@users.noreply.github.com>
2021-08-27 13:19:19 +0530

    Updated file ex2 with B=100 and C=200

--- ex2-dfunction.py
+++ ex2-dfunction.py
@@ -16,3 +16,6 @@ print("Added this line from the GitHub website for the first time")
 #----------------------------------------------------
 print("Added this line from User-2 account")
 y = 200
+print("Added this line from User-2 account")
+B = 100
+C = 200
