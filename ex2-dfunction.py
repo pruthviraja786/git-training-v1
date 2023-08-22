87e723983aebb7ff7f102e848760ce91d70d1e88
Fiacotech <142477106+Fiacotech@users.noreply.github.com>
2023-08-22 22:57:55 +0100

    Update ex2-dfunction.py
    Update ex2-dfunction.py with C = 300 and g = 200

--- ex2-dfunction.py
+++ ex2-dfunction.py
@@ -18,7 +18,7 @@ print("Added this line from User-2 account")
 y = 200
 print("Added this line from User-2 account")
 B = 100
-C = 200
+C = 300
 
 print("Added this line from fiaco User-2 account")
-g = 100
+g = 200
