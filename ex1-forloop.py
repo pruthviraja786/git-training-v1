032d9acd938a37644bac52d09f6db83c5f2ed3b1
Fiacotech <142477106+Fiacotech@users.noreply.github.com>
2023-08-22 22:53:33 +0100

    Update ex1-forloop.py with g = 700 and h = 900
    Update ex1-forloop.py with g = 700 and h = 900 from Github User-2-account

--- ex1-forloop.py
+++ ex1-forloop.py
@@ -25,4 +25,9 @@ C = 900
 
 print("Added this line from fiaco User-2 account")
 f = 100
+print("Added this line from fiaco User-2 account")
+g = 700
+
+print("Added this line from fiaco User-2 account")
+h = 500
 
